1d5884f2100a9478057b80586dcc01e56b5a6968
Jason Pierce <jason@2toad.com>
2017-01-29 22:12:15 -0800

    Update unit tests

--- Rijndael256.Tests/RijndaelEtMTests.cs
+++ Rijndael256.Tests/RijndaelEtMTests.cs
@@ -7,6 +7,7 @@
  */
 
 using System;
+using System.Linq;
 using System.Text;
 using Xunit;
 
@@ -17,16 +18,26 @@ namespace Rijndael256.Tests
         const string Password = "Tomatoe Belize Gestalt Sierra";
         static readonly byte[] Iv = Encoding.UTF8.GetBytes("D6BB4C14C3BF491F");
         const string Plaintext = "A secret phrase to test AES using AE Encrypt-then-MAC mode.";
-        const string Proof128 = "RDZCQjRDMTRDM0JGNDkxRtS+RAdwVMS6M5pj72jp1pV0aVj4QvZ5EBWQgwzSvF3i5K9PiI8BoV4X0jg9+kBcJu1YJCStyy+GhUfCIpeFoIakL43pla1AmYtw4s5ZK2Vst+eWsrcRgz/087FjfNtWwAqd1OtH8CgNkpOK+7WD9uP2hBK3fYnameaK/egB4Xdc";
-        const string Proof192 = "RDZCQjRDMTRDM0JGNDkxRnUSZIKdb+0yFIiQdG2z9DHcqEWBv6TLuJGo4iQdRPcWjcIh+xjU3MxAel+1V6rIT2QsqyEHgDj/UmJinEwEn0JzV9tor4K0mS/ELRL1ziIs8DeNOXmDJdL2N5ivz6Fx1vahgiXDCG4jbZfwzLexG/yOIsFdN5EDSIGmyVEbRV0g";
-        const string Proof256 = "RDZCQjRDMTRDM0JGNDkxRtwcUOKuBaOOpLPY2hUdMZC9flT2NF9oOVCGTb0kGugKZ7XYd/AlHG1JbBwcfmfGffkcP8tsrc02aBUNwSXzMP4ssvTFAdeOFgahnomVwL1W2Gbhd7bqTKIrptYzBPlKDOWOvAPWBvd4KLctZMJ2bkKZMwenqCXNq3FfNXxvggag";
+
+        const string EtmProof128 = "RDZCQjRDMTRDM0JGNDkxRtS+RAdwVMS6M5pj72jp1pV0aVj4QvZ5EBWQgwzSvF3i5K9PiI8BoV4X0jg9+kBcJu1YJCStyy+GhUfCIpeFoIakL43pla1AmYtw4s5ZK2Vst+eWsrcRgz/087FjfNtWwAqd1OtH8CgNkpOK+7WD9uP2hBK3fYnameaK/egB4Xdc";
+        const string EtmProof192 = "RDZCQjRDMTRDM0JGNDkxRnUSZIKdb+0yFIiQdG2z9DHcqEWBv6TLuJGo4iQdRPcWjcIh+xjU3MxAel+1V6rIT2QsqyEHgDj/UmJinEwEn0JzV9tor4K0mS/ELRL1ziIs8DeNOXmDJdL2N5ivz6Fx1vahgiXDCG4jbZfwzLexG/yOIsFdN5EDSIGmyVEbRV0g";
+        const string EtmProof256 = "RDZCQjRDMTRDM0JGNDkxRtwcUOKuBaOOpLPY2hUdMZC9flT2NF9oOVCGTb0kGugKZ7XYd/AlHG1JbBwcfmfGffkcP8tsrc02aBUNwSXzMP4ssvTFAdeOFgahnomVwL1W2Gbhd7bqTKIrptYzBPlKDOWOvAPWBvd4KLctZMJ2bkKZMwenqCXNq3FfNXxvggag";
+
+        static readonly byte[] MacProof128 = Convert.FromBase64String("Mj88Yf3cfDOY+pnjcpVKwbaITgF25hPORX2H+lETXuz5AoIaEyZ0f9a1Enf1WK2VMHlb75DW5STs9z8NaT96bA==");
+        static readonly byte[] MacProof192 = Convert.FromBase64String("20SB6UTQZmIcrHROvR7UIrIWlQ6VEDYmJy0ZI/iO6fJh6JOyPPEADsGvHPVfWlSbB/dlwu8NKD5xf3uF61mswA==");
+        static readonly byte[] MacProof256 = Convert.FromBase64String("BHpBmrYfKRN/Xg1ZULOWMD7naD/dgydjKXPhQIauOtgnDXI6HPY+N1C6IrjKP0JbFfMbGVuoADA3eC2q/ZcX2g==");
+
+        static readonly AuthKeys AuthKeysProof = new AuthKeys {
+            CipherKey = "238FB434AE3ADCFFB6C02CD7275EB3847A957E1726AC55204C6CF8DF1F0EA2D8",
+            MacKey = Convert.FromBase64String("MkQ0NTM2MzYyOUExM0I2MDVCQjUyNjNFNkY0N0U4OEVBOTk5NEI1RDA0NkZCQTA5NUI1QjQzQTkwODY0RDA2Mg==")
+        };
 
         [Fact]
         public void Encrypt128()
         {
             var plaintext = Encoding.UTF8.GetBytes(Plaintext);
             var etmCiphertext = RijndaelEtM.Encrypt(plaintext, Password, Iv, KeySize.Aes128);
-            Assert.Equal(Convert.ToBase64String(etmCiphertext), Proof128);
+            Assert.Equal(Convert.ToBase64String(etmCiphertext), EtmProof128);
         }
 
         [Fact]
@@ -34,7 +45,7 @@ namespace Rijndael256.Tests
         {
             var plaintext = Encoding.UTF8.GetBytes(Plaintext);
             var etmCiphertext = RijndaelEtM.Encrypt(plaintext, Password, Iv, KeySize.Aes192);
-            Assert.Equal(Convert.ToBase64String(etmCiphertext), Proof192);
+            Assert.Equal(Convert.ToBase64String(etmCiphertext), EtmProof192);
         }
 
         [Fact]
@@ -42,27 +53,27 @@ namespace Rijndael256.Tests
         {
             var plaintext = Encoding.UTF8.GetBytes(Plaintext);
             var etmCiphertext = RijndaelEtM.Encrypt(plaintext, Password, Iv, KeySize.Aes256);
-            Assert.Equal(Convert.ToBase64String(etmCiphertext), Proof256);
+            Assert.Equal(Convert.ToBase64String(etmCiphertext), EtmProof256);
         }
 
         [Fact]
         public void Decrypt128()
         {
-            var plaintext = RijndaelEtM.Decrypt(Proof128, Password, KeySize.Aes128);
+            var plaintext = RijndaelEtM.Decrypt(EtmProof128, Password, KeySize.Aes128);
             Assert.Equal(plaintext, Plaintext);
         }
 
         [Fact]
         public void Decrypt192()
         {
-            var plaintext = RijndaelEtM.Decrypt(Proof192, Password, KeySize.Aes192);
+            var plaintext = RijndaelEtM.Decrypt(EtmProof192, Password, KeySize.Aes192);
             Assert.Equal(plaintext, Plaintext);
         }
 
         [Fact]
         public void Decrypt256()
         {
-            var plaintext = RijndaelEtM.Decrypt(Proof256, Password, KeySize.Aes256);
+            var plaintext = RijndaelEtM.Decrypt(EtmProof256, Password, KeySize.Aes256);
             Assert.Equal(plaintext, Plaintext);
         }
 
@@ -97,6 +108,39 @@ namespace Rijndael256.Tests
 
             Assert.Equal(plaintext, Plaintext);
             Assert.NotEqual(etmCiphertext1, etmCiphertext2);
+        }
+
+        [Fact]
+        public void CalculateMac128()
+        {
+            var proof128 = Convert.FromBase64String(EtmProof128);
+            var mac = RijndaelEtM.CalculateMac(proof128, Iv);
+            Assert.True(mac.SequenceEqual(MacProof128));
+        }
+
+        [Fact]
+        public void CalculateMac192()
+        {
+            var proof192 = Convert.FromBase64String(EtmProof192);
+            var mac = RijndaelEtM.CalculateMac(proof192, Iv);
+            Assert.True(mac.SequenceEqual(MacProof192));
+        }
+
+        [Fact]
+        public void CalculateMac256()
+        {
+            var proof256 = Convert.FromBase64String(EtmProof256);
+            var mac = RijndaelEtM.CalculateMac(proof256, Iv);
+            Assert.True(mac.SequenceEqual(MacProof256));
+        }
+
+        [Fact]
+        public void GenerateAuthKeys()
+        {
+            var keys = AuthKeys.Generate(Password);
+
+            Assert.Equal(keys.CipherKey, AuthKeysProof.CipherKey);
+            Assert.True(keys.MacKey.SequenceEqual(AuthKeysProof.MacKey));
         }
     }
 }
