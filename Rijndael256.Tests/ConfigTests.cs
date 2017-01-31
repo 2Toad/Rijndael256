b8f464d5f1189cac2b35355ada597e8c7942f25c
Jason Pierce <jason@2toad.com>
2017-01-31 13:57:24 -0800

    Fixes #1: Add configurable iterations for Rijndael and RijndaelEtM hashes

---
+++ Rijndael256.Tests/ConfigTests.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Rijndael256
+ * Copyright (C)2013 2Toad, LLC.
+ * licensing@2toad.com
+ * 
+ * https://github.com/2Toad/Rijndael256
+ */
+
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Rijndael256.Tests
+{
+    public class ConfigTests
+    {
+        const string Plaintext = "A secret phrase to test hashing.";
+        const string Salt = "0A9FDB669FA44FF1BEC484A1BE6B6E2A";
+
+        [Fact]
+        public void HashIterations()
+        {
+            var proofDefault = Convert.FromBase64String("pGnGDWwZAVvIlmDgGZ1gkvqEqm2DvkYdPRygUjZGIW/Ts+q2R+ZdMlRzfV1Dlz4udcHwS+A/1TjP+6jBDUTBMQ==");
+
+            var hashDefault = Hash.Pbkdf2(Plaintext, Salt, Config.HashIterations);
+            Assert.True(hashDefault.SequenceEqual(proofDefault));
+
+            Config.HashIterations = 500;
+
+            var hashChanged = Hash.Pbkdf2(Plaintext, Salt, Config.HashIterations);
+            Assert.False(hashChanged.SequenceEqual(proofDefault));
+
+            RestoreDefaults();
+        }
+
+        /// <summary>
+        /// Config is global, so we need to restore defaults before the other
+        /// unit tests, which depend on said defaults, are run.
+        /// </summary>
+        private void RestoreDefaults()
+        {
+            Config.HashIterations = 10000;
+        }
+    }
+}
---
+++ Rijndael256/Config.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Rijndael256
+ * Copyright (C)2013 2Toad, LLC.
+ * licensing@2toad.com
+ * 
+ * https://github.com/2Toad/Rijndael256
+ */
+
+namespace Rijndael256
+{
+    /// <summary>
+    /// Global configurations
+    /// </summary>
+    public static class Config
+    {
+        /// <summary>
+        /// The number of iterations used to derive hashes.
+        /// Default is 10000.
+        /// </summary>
+        public static int HashIterations = 10000;
+    }
+}
